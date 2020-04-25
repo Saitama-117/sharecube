85a4e4c31644722129d65eda3d77158077834327
Saitama-117 <60344345+Saitama-117@users.noreply.github.com>
2020-04-25 19:39:47 +0100

    Add files via upload

---
+++ GuestBar.php
@@ -0,0 +1,36 @@
+<style>
+    li
+    {
+      color:green;
+    }
+
+   
+     
+</style>
+
+<nav class="navbar navbar-default navbar-fixed-top" style="background-color:black;color:white;">
+  <div class="navbar-header">
+      <a href="index.php"><img src="images/sharebox.png" id="Cube" sytle="marging-right:100px," width=50%></a><!--Allow user go back to main page-->
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#nav-menu" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+  </div>
+  <div id="nav-menu" class="collapse navbar-collapse navbar-right" >
+    <ul class="nav navbar-nav">          
+      <li class="dropdown">
+        <a style='color:#ffffff;' class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" href="help.php">
+          <strong id="about"> About</strong><b class="caret"></b><!--Drop list Menu-->
+        </a>
+        <ul class="dropdown-menu">
+          <li><a style="padding-top:8px;padding-bottom:8px;" href="index.php">Home</a></li>
+          <li><a style="padding-top:8px;padding-bottom:8px;" href="help.php#introduction">About</a></li>
+          <li><a style="padding-top:8px;padding-bottom:8px;" href="help.php#contactus">Contact Us</a></li>  
+        </ul>
+      </li>
+              
+    </ul>
+  </div>
+</nav>
---
+++ footer.php
@@ -0,0 +1,31 @@
+
+ <footer style="  bottom:0; margin-top:150px">
+
+
+     <div class="container-fluid container-float container-fixed-bottom" >
+         <div class="row">
+             <div class="col-sm-12" ">
+                 <p>
+                     <br/>
+                     &copy<?php echo Date('Y'); ?> ShareCube. Sharing knowledge.
+                    <a  href="https://www.facebook.com/" target="_blanc" ><i  class="fa fa-facebook-official w3-hover-opacity"></i></a>
+                    <a href="https://www.instagram.com/" target="_blanc"><i class="fa fa-instagram w3-hover-opacity"></i></a>
+                    <a href="https://twitter.com/explore" target="_blanc" ><i class="fa fa-twitter w3-hover-opacity"></i></a>
+                    <a href="https://www.linkedin.com/" target="_blanc" ><i class="fa fa-linkedin w3-hover-opacity"></i></a>
+                 </p>
+
+             </div>
+         </div>
+
+     </div>
+
+ </footer>
+    
+ <script src="js/jquery-1.11.3.js"></script>
+ <script src="js/bootstrap.js"></script>
+ <script type='text/javascript' src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.5.0/js/bootstrap-datepicker.min.js"></script>
+ <script type="text/javascript" src="js/bootstrap-clockpicker.min.js"></script>
+ <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAqeUirCne4O48i2Fqk9iz7ERFlpgd2qts&libraries=places&callback=initAutocomplete" async defer></script>
+
+ </body>
+ </html>
